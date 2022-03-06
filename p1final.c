1923616958df6a36217cce11c756d50113bf488a
vpt03 <97585809+Vpt03@users.noreply.github.com>
2022-03-06 06:46:53 +0000

    p1 done

--- p1final.c
+++ p1final.c
@@ -1,1 +1,27 @@
-#include <stdio.h>
+#include<stdio.h>
+
+void input(int *num1, int *den1, int *num2, int *den2)
+{
+  printf("Enter two fraction numbers");
+  scanf("%d%d",num1,den1);
+  scanf("%d%d",num2,den2);
+}
+void add(int num1,int den1, int num2, int den2, int *num3, int *den3)
+{
+  num1 = num1 * den2;
+  num2 = num2 * den1;
+  *num3 = num1 + num2;
+  *den3=den1 * den2;
+}
+void output(int num1, int den1, int num2, int den2, int num3, int den3)
+{
+  printf("Sum of two given fractions is %d/%d+%d/%d=%d/%d\n",num1,den1,num2,den2,num3,den3);
+}
+int main()
+{
+  int num1,num2,num3,den1,den2,den3;
+  input(&num1,&den1,&num2,&den2);
+  add(num1,den1,num2,den2,&num3,&den3);
+  output(num1,den1,num2,den2,num3,den3);
+  return 0;
+}
