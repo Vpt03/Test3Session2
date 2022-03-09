65f22643a5183c564416ba9799b334c2fc8754b2
vpt03 <97585809+Vpt03@users.noreply.github.com>
2022-03-09 03:43:38 +0000

    p7 done

--- p7final.c
+++ p7final.c
@@ -1,12 +1,19 @@
 #include<stdio.h>
-struct _fraction
+typedef struct _fraction
 {
-  int num,den;
-};
-typedef struct _fraction Fraction;
-  int gcd(int a,int b)
-  {
-    int t;
+int num,den;
+}Fraction;
+
+Fraction input_fraction()
+{
+  Fraction f;
+  printf("input a fraction\n");
+  scanf("%d%d",&f.num,&f.den);
+  return f;
+}
+int gcd(int a,int b)
+{
+  int t;
     while (b != 0)
       {
         t = b;
@@ -15,7 +22,26 @@ typedef struct _fraction Fraction;
       }
     return a;
 }
+Fraction add_fraction(Fraction f1,Fraction f2)
+{
+  Fraction sum;
+  sum.num=(f1.num)*(f2.den) + (f2.num)*(f1.den);
+  sum.den=(f1.den)*(f2.den);
+  int g= gcd(sum.num,sum.den);
+  sum.num=sum.num/g;
+  sum.den=sum.den/g;
+  return sum;
+}
+void output(Fraction f1,Fraction f2,Fraction sum)
+{
+  printf("Sum of %d/%d + %d/%d = %d/%d",f1.num,f1.den,f2.num,f2.den,sum.num,sum.den);
+}
 int main()
 {
-  printf("%d\n",gcd(16,24));
+  Fraction f1,f2,sum;
+  f1=input_fraction();
+  f2=input_fraction();
+  sum=add_fraction(f1,f2);
+  output(f1,f2,sum);
+  return 0;
 }
